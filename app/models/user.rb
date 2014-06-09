d57d046630021cbfffc964e4e4131caa126e9163
Paulo Toro <pauloat@gmail.com>
2014-06-09 19:32:08 -0300

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,17 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :microposts, dependent: :destroy
-  
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
+
   before_save { email.downcase! }
   before_create :create_remember_token
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i 
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: {
-    case_sensitive: false } 
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false } 
   validates :password, length: { minimum: 6 }
 
   def User.new_remember_token
@@ -20,10 +23,21 @@ class User < ActiveRecord::Base
   end
 
   def feed
-    # This is preliminary. see "Followig users" for the full implementation.
-    Micropost.where("user_id = ?", id)
+    Micropost.from_users_followed_by(self)
+  end
+
+  def following?(other_user)
+    relationships.find_by(followed_id: other_user.id)
   end
   
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by(followed_id: other_user.id).destroy
+  end
+
   private
 
     def create_remember_token
